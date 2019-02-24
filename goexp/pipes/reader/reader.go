bec48d0c79611c5df09348adbf012088edb3da0c
Monmohan Singh <monmohan@gmail.com>
2019-02-24 23:59:19 +0800

    connected reader writer

--- goexp/pipes/reader/reader.go
+++ goexp/pipes/reader/reader.go
@@ -9,19 +9,31 @@ import (
 	"syscall"
 )
 
-func main() {
+func goNativeImpl() {
 	namedPipe := filepath.Join("/home/singhmo/named-pipes", "ABCD")
 	err := syscall.Mkfifo(namedPipe, 0600)
 	if err != nil {
 		fmt.Printf("Error creating fifo named pipe %v\n", err.Error())
 	}
 	np, _ := os.OpenFile(namedPipe, os.O_RDONLY, 0600)
+	defer np.Close()
 	var b bytes.Buffer
-	n, err := io.Copy(&b, np)
-	if err != nil {
-		fmt.Printf("Error reading fifo named pipe %v\n", err.Error())
+	for {
+		n, err := io.CopyN(&b, np, 10)
+		if err != nil {
+			fmt.Printf("Error reading fifo named pipe %v\n", err.Error())
+			return
+		}
+		fmt.Printf("read %d bytes from pipe \n", n)
+		fmt.Printf("\n%s\n", b.String())
+		b.Reset()
 	}
-	fmt.Printf("read %d bytes from pipe \n", n)
-	np.Close()
-	fmt.Printf("Data %s", b.String())
+}
+
+func dockerImpl() {
+
+}
+
+func main() {
+	goNativeImpl()
 }
--- goexp/pipes/writer/writer.go
+++ goexp/pipes/writer/writer.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func main() {
 	namedPipe := filepath.Join("/home/singhmo/named-pipes", "ABCD")
 	np, _ := os.OpenFile(namedPipe, os.O_WRONLY, 0600)
-	np.Write([]byte("Hello"))
-	np.Close()
+	defer np.Close()
+	i := 1
+	for {
+		fmt.Printf("Wrote %d times\n", i)
+		np.Write([]byte("Hello"))
+		time.Sleep(10 * time.Second)
+		i++
+	}
+
 }
